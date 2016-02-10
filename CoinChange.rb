83eb975919e2f1d7ecab1186a6f3ea149ee4c584
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-02-10 14:42:50 -0600

    Refactored to more than 1 method.

--- CoinChange.rb
+++ CoinChange.rb
@@ -8,30 +8,41 @@ class Amount
 		if @value == 0
 			return 0
 		elsif @value.to_i >= 25
-			if @value.to_i % 25 == 0
-			    return (@value.to_i / 25)
-			else
-				new_value = @value.to_i - 25
-				return (Amount.new(new_value).coins) + 1
-			end
+			quarter
 		elsif @value.to_i >= 10
-			if @value.to_i % 10 == 0
-    			return (@value.to_i / 10)
-    		else
-    			new_value = @value.to_i - 10
-				return (Amount.new(new_value).coins) + 1
-			end
+			dime
 		elsif @value.to_i >= 5
-			if @value.to_i % 5 == 0
-    			return (@value.to_i / 5)
-    		else
-    			new_value = @value.to_i - 5
-				return (Amount.new(new_value).coins) + 1
-			end
+			nickel
 		else
 			return @value.to_i
 		end
+	end
 
+	def quarter
+		if @value.to_i % 25 == 0
+		    return (@value.to_i / 25)
+		else
+			new_value = @value.to_i - 25
+			return (Amount.new(new_value).coins) + 1
+		end
+	end
+
+	def dime
+		if @value.to_i % 10 == 0
+    		return (@value.to_i / 10)
+   		else
+    		new_value = @value.to_i - 10
+			return (Amount.new(new_value).coins) + 1
+		end
+	end
+
+	def nickel
+		if @value.to_i % 5 == 0
+			return (@value.to_i / 5)
+		else
+			new_value = @value.to_i - 5
+			return (Amount.new(new_value).coins) + 1
+		end
 	end
 
 end
